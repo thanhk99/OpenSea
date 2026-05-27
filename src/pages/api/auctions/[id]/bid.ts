09cb360ce0575244270da11a2abc431ed30fb722
Nguyen An <thamjsjsjs@gmail.com>
2026-05-27 15:33:24 +0700

    update

--- src/pages/api/auctions/[id]/bid.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { proxyToBackend } from '../../_proxy'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query
-  return proxyToBackend(req, res, `/api/auctions/${String(id)}/bid`)
-}
--- src/pages/api/nfts.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { proxyToBackend } from './_proxy'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return proxyToBackend(req, res, '/api/nfts')
-}
